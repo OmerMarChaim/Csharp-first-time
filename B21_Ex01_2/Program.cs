08dfcf7b37e30653221d84775e4367387d37f6a2
Omer MarChaim <omermar7@gmail.com>
2021-04-06 18:21:27 +0300

    start of ex3
    there is a problem with namespace

--- B21_Ex01_2/Program.cs
+++ B21_Ex01_2/Program.cs
@@ -3,16 +3,16 @@ using System.Text;
 
 namespace B21_Ex01_2
 {
-    class Program
+   public class Program
     {
         public static void Main()
         {
             StringBuilder highLevelAtSandClock = new StringBuilder();
             highLevelAtSandClock.Append("*****");
-            recursionSandClock(5, new StringBuilder(),highLevelAtSandClock );
+            RecursionSandClock(5, new StringBuilder(),highLevelAtSandClock );
         }
 
-        private static void recursionSandClock(int i_HeightLevel, StringBuilder i_StringOfSpaces, StringBuilder i_StringOfStars)
+        public static void RecursionSandClock(int i_HeightLevel, StringBuilder i_StringOfSpaces, StringBuilder i_StringOfStars)
         {
             if(i_HeightLevel == 1)
             {
@@ -25,7 +25,7 @@ namespace B21_Ex01_2
             i_StringOfSpaces.Append(" ");
             i_StringOfStars.Remove(0, 2);
 
-            recursionSandClock(i_HeightLevel -= 2, i_StringOfSpaces, i_StringOfStars);
+            RecursionSandClock(i_HeightLevel -= 2, i_StringOfSpaces, i_StringOfStars);
             i_StringOfSpaces.Remove(0, 1);
             i_StringOfStars.Append("**");
             Console.WriteLine("{0}{1}{0}", i_StringOfSpaces, i_StringOfStars);
--- B21_Ex01_3/Program.cs
+++ B21_Ex01_3/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+using B21_Ex01_2;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace B21_Ex01_3
 {
-    class Program
+    public class Program
     {
+        public static void Main()
+        {
+            Console.WriteLine("Write the hieght of the sand clock you wish to see");
+            String stringNum = Console.ReadLine();
+            int highOfSandCLock = int.Parse(stringNum);
+            if(highOfSandCLock % 2 == 0)
+            {
+                highOfSandCLock--;
+            }
+
+            StringBuilder firstLine = new StringBuilder();
+            for(int i = 0; i < highOfSandCLock; i++)
+            {
+                firstLine.Append("*");
+            }
+            
+
+        }
     }
 }
