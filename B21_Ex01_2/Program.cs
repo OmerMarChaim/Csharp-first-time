8bd116d167394a59885b4c3da9ebce94acd200a9
Omri Cohen <omricohen101@gmail.com>
2021-04-06 21:05:15 +0300

    Ex1_04 after format

--- B21_Ex01_2/Program.cs
+++ B21_Ex01_2/Program.cs
@@ -3,17 +3,21 @@ using System.Text;
 
 namespace B21_Ex01_2
 {
-   public class Program
+    public static class Program
     {
         public static void Main()
         {
             StringBuilder highLevelAtSandClock = new StringBuilder();
             highLevelAtSandClock.Append("*****");
-            RecursionSandClock(5, new StringBuilder(),highLevelAtSandClock );
+            RecursionSandClock(5, new StringBuilder(), highLevelAtSandClock);
         }
-        public static void RecursionSandClock(int i_HeightLevel, StringBuilder i_StringOfSpaces, StringBuilder i_StringOfStars)
+
+        public static void RecursionSandClock(
+            int i_HeightLevel,
+            StringBuilder i_StringOfSpaces,
+            StringBuilder i_StringOfStars)
         {
-            if (i_HeightLevel == 1)
+            if(i_HeightLevel == 1)
             {
                 Console.WriteLine("{0}{1}{0}", i_StringOfSpaces, i_StringOfStars);
 
@@ -24,11 +28,10 @@ namespace B21_Ex01_2
             i_StringOfSpaces.Append(" ");
             i_StringOfStars.Remove(0, 2);
 
-            RecursionSandClock(i_HeightLevel -= 2, i_StringOfSpaces, i_StringOfStars);
+            RecursionSandClock(i_HeightLevel - 2, i_StringOfSpaces, i_StringOfStars);
             i_StringOfSpaces.Remove(0, 1);
             i_StringOfStars.Append("**");
             Console.WriteLine("{0}{1}{0}", i_StringOfSpaces, i_StringOfStars);
         }
-
     }
 }
