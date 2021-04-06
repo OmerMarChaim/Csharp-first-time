3eeacc7ac581a29283f794094c62e31084b09a7f
Omri Cohen <omricohen101@gmail.com>
2021-04-06 19:18:13 +0300

    Ex1_03 getValidinput() and some fixes in Main()

--- B21_Ex01_3/Program.cs
+++ B21_Ex01_3/Program.cs
@@ -1,33 +1,39 @@
-﻿using B21_Ex01_2;
-using System;
+﻿using System;
 using System.Text;
 
 namespace B21_Ex01_3
 {
-    public class Program
+    public static class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Write the hieght of the sand clock you wish to see");
-            String stringNum = getValidinput();
-            int highOfSandCLock = int.Parse(stringNum);
-            if(highOfSandCLock % 2 == 0)
+            Console.WriteLine("Write the height of the sand clock you wish to see");
+            int highOfSandCLockInput = getValidInput();
+            if(highOfSandCLockInput % 2 == 0)
             {
-                highOfSandCLock--;
+                highOfSandCLockInput--;
             }
 
             StringBuilder firstLine = new StringBuilder();
-            for(int i = 0; i < highOfSandCLock; i++)
+            for(int i = 0; i < highOfSandCLockInput; i++)
             {
                 firstLine.Append("*");
             }
 
-            B21_Ex01_2.Program.RecursionSandClock(highOfSandCLock, new StringBuilder(), firstLine);
+            B21_Ex01_2.Program.RecursionSandClock(highOfSandCLockInput, new StringBuilder(), firstLine);
         }
 
-        // private static string getValidinput()
-        // {
-        //     Console.ReadLine()
-        // }
+        private static int getValidInput()
+        {
+            int result;
+            string userInput = Console.ReadLine();
+            while(!(int.TryParse(userInput, out result) && result > 0 ))
+            {
+                Console.WriteLine("You entered invalid input. Please insert positive number");
+                userInput = Console.ReadLine();
+            }
+
+            return result;
+        }
     }
 }
