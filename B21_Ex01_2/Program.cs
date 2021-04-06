00e9fcb4052acb477b2795a7fc4bb7c3795e069b
Omri Cohen <omricohen101@gmail.com>
2021-04-06 18:04:17 +0300

    Ex1_02 remove Assigment of HeightLevel

--- B21_Ex01_2/Program.cs
+++ B21_Ex01_2/Program.cs
@@ -9,10 +9,13 @@ namespace B21_Ex01_2
         {
             StringBuilder highLevelAtSandClock = new StringBuilder();
             highLevelAtSandClock.Append("*****");
-            recursionSandClock(5, new StringBuilder(),highLevelAtSandClock );
+            recursionSandClock(5, new StringBuilder(), highLevelAtSandClock);
         }
 
-        private static void recursionSandClock(int i_HeightLevel, StringBuilder i_StringOfSpaces, StringBuilder i_StringOfStars)
+        private static void recursionSandClock(
+            int i_HeightLevel,
+            StringBuilder i_StringOfSpaces,
+            StringBuilder i_StringOfStars)
         {
             if(i_HeightLevel == 1)
             {
@@ -20,16 +23,15 @@ namespace B21_Ex01_2
 
                 return;
             }
-            
+
             Console.WriteLine("{0}{1}{0}", i_StringOfSpaces, i_StringOfStars);
             i_StringOfSpaces.Append(" ");
             i_StringOfStars.Remove(0, 2);
 
-            recursionSandClock(i_HeightLevel -= 2, i_StringOfSpaces, i_StringOfStars);
+            recursionSandClock(i_HeightLevel - 2, i_StringOfSpaces, i_StringOfStars);
             i_StringOfSpaces.Remove(0, 1);
             i_StringOfStars.Append("**");
             Console.WriteLine("{0}{1}{0}", i_StringOfSpaces, i_StringOfStars);
-            
         }
     }
 }
