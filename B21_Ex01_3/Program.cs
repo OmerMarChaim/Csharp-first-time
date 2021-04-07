748c44cb797fa1428853bb28c0b281cbafd24886
Omri Cohen <omricohen101@gmail.com>
2021-04-07 15:33:20 +0300

    Ex01_4 getValidInputs

--- B21_Ex01_3/Program.cs
+++ B21_Ex01_3/Program.cs
@@ -10,25 +10,25 @@ namespace B21_Ex01_3
         {
             Console.WriteLine("Write the height of the sand clock you wish to see");
             int highOfSandCLockInput = getValidInput();
-            if (highOfSandCLockInput % 2 == 0)
+            if(highOfSandCLockInput % 2 == 0)
             {
                 highOfSandCLockInput--;
             }
 
             StringBuilder firstLine = new StringBuilder();
-            for (int i = 0; i < highOfSandCLockInput; i++)
+            for(int i = 0; i < highOfSandCLockInput; i++)
             {
                 firstLine.Append("*");
             }
 
-               B21_Ex01_2.Program.RecursionSandClock(highOfSandCLockInput, new StringBuilder(), firstLine);
+            B21_Ex01_2.Program.RecursionSandClock(highOfSandCLockInput, new StringBuilder(), firstLine);
         }
 
         private static int getValidInput()
         {
             int result;
             string userInput = Console.ReadLine();
-            while (!(int.TryParse(userInput, out result) && result > 0))
+            while(!(int.TryParse(userInput, out result) && result > 0))
             {
                 Console.WriteLine("You entered invalid input. Please insert positive number");
                 userInput = Console.ReadLine();
--- Ex1_B21_4/Program.cs
+++ Ex1_B21_4/Program.cs
@@ -7,29 +7,36 @@ namespace Ex1_B21_4
     {
         public static void Main()
         {
-            String input = validInputUser();
+            String input = getValidInput();
             stringAnalise(input);
         }
 
-        private static string validInputUser()
+        private static string getValidInput()
         {
-            string validInput;
+            Console.WriteLine("Please enter a valid input: consist of English letters only or digits only");
+            string userInput = Console.ReadLine();
+            while(isNumber(userInput) || isEnglishWord(userInput))
+            {
+                Console.WriteLine("Your input is invalid. Pleas try again.");
+                userInput = Console.ReadLine();
+            }
 
-            return validInput;
+            return userInput;
         }
 
         private static void stringAnalise(string i_Input)
         {
             bool isPalindromFlag = isPalindrome(i_Input);
-            if(isNumber())
+            if(isNumber(i_Input))
             {
                 bool isMultipalOfFourFlag = isMultipalOfFour(i_Input);
             }
             else
             {
-                Console.WriteLine("The number of capital letters (uppercase) in your input is {0}",numberOfCapitalLetters(i_Input));
+                Console.WriteLine(
+                    "The number of capital letters (uppercase) in your input is {0}",
+                    numberOfCapitalLetters(i_Input));
             }
-            
         }
 
         private static int numberOfCapitalLetters(string i_Input)
@@ -42,9 +49,25 @@ namespace Ex1_B21_4
             throw new NotImplementedException();
         }
 
-        private static bool isNumber()
+        private static bool isNumber(String i_String)
         {
-            throw new NotImplementedException();
+            return int.TryParse(i_String, out int _);
+        }
+
+        private static bool isEnglishWord(string i_UserInput)
+        {
+            bool isEnglishWordFlag = true;
+            for(int i = 0; i < i_UserInput.Length; i++)
+            {
+                if(!(Char.IsLower(i_UserInput[i]) || Char.IsUpper(i_UserInput[i])))
+                {
+                    isEnglishWordFlag = false;
+
+                    break;
+                }
+            }
+
+            return isEnglishWordFlag;
         }
 
         private static bool isPalindrome(string i_Input)
