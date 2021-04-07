c0a866034e3b9750cb15c39c75d908a8a8adedd9
Omri Cohen <omricohen101@gmail.com>
2021-04-07 14:58:20 +0300

    Ex01_4 start with TODO'S

--- Ex1_B21_4/Program.cs
+++ Ex1_B21_4/Program.cs
@@ -3,23 +3,53 @@ using System.Text;
 
 namespace Ex1_B21_4
 {
-    class Program
+    public static class Program
     {
-        public static void main()
+        public static void Main()
         {
-            String input = validInputQuestion4();
+            String input = validInputUser();
             stringAnalise(input);
         }
 
-        private static string validInputQuestion4()
+        private static string validInputUser()
         {
-            string validInput ;
+            string validInput;
+
             return validInput;
         }
 
         private static void stringAnalise(string i_Input)
         {
+            bool isPalindromFlag = isPalindrome(i_Input);
+            if(isNumber())
+            {
+                bool isMultipalOfFourFlag = isMultipalOfFour(i_Input);
+            }
+            else
+            {
+                Console.WriteLine("The number of capital letters (uppercase) in your input is {0}",numberOfCapitalLetters(i_Input));
+            }
             
+        }
+
+        private static int numberOfCapitalLetters(string i_Input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool isMultipalOfFour(string i_Input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool isNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool isPalindrome(string i_Input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
