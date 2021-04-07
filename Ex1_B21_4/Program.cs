6e2aa1460f2c0355debf889defeabd40d7837668
Omer MarChaim <omermar7@gmail.com>
2021-04-07 15:34:22 +0300

    ex4 fanction without valid input

--- Ex1_B21_4/Program.cs
+++ Ex1_B21_4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace Ex1_B21_4
@@ -7,20 +8,21 @@ namespace Ex1_B21_4
     {
         public static void Main()
         {
-            String input = validInputUser();
-            stringAnalise(input);
+           String input = validInputUser();
+           stringAnalise(input);
         }
 
-        private static string validInputUser()
-        {
-            string validInput;
+      //  private static string validInputUser()
+      //  {
+        //    string validInput;
 
-            return validInput;
-        }
+           // return validInput;
+    //    }
 
         private static void stringAnalise(string i_Input)
         {
             bool isPalindromFlag = isPalindrome(i_Input);
+            Console.WriteLine("this is a palindrom{0}",isPalindromFlag);
             if(isNumber())
             {
                 bool isMultipalOfFourFlag = isMultipalOfFour(i_Input);
@@ -34,12 +36,22 @@ namespace Ex1_B21_4
 
         private static int numberOfCapitalLetters(string i_Input)
         {
-            throw new NotImplementedException();
+            int numOfCapital = 0;
+            foreach(char t in i_Input)
+            {
+                if(t >= 'A' && t <= 'Z')
+                {
+                    numOfCapital++;
+                }
+            }
+            return numOfCapital;
+
         }
 
         private static bool isMultipalOfFour(string i_Input)
         {
-            throw new NotImplementedException();
+            int numberRepInput = int.Parse(i_Input);
+            return numberRepInput % 4 == 0 ? true : false;
         }
 
         private static bool isNumber()
@@ -49,7 +61,27 @@ namespace Ex1_B21_4
 
         private static bool isPalindrome(string i_Input)
         {
-            throw new NotImplementedException();
+            bool palindromeFlag=false;
+            return isPalindromeReqHelper(i_Input,0, (i_Input.Length)-1,palindromeFlag);
+        }
+
+        private static bool isPalindromeReqHelper(string i_Input, int i_LeftEdge, int i_RightEdge,bool palindromeFlag)
+        {
+            
+            if(i_LeftEdge > i_RightEdge)
+            {
+                palindromeFlag = true;
+                return palindromeFlag;
+            }
+
+            if(i_Input[i_LeftEdge] != i_Input[i_RightEdge])
+            {
+                palindromeFlag = false;
+                return palindromeFlag;
+            }
+
+            isPalindromeReqHelper(i_Input, i_LeftEdge++, i_RightEdge--,palindromeFlag);
+            return palindromeFlag;
         }
     }
 }
