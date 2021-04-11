1c971ac33a5d8efd29b3c24018ec3f3884440820
Omri Cohen <omricohen101@gmail.com>
2021-04-11 14:51:21 +0300

    before start Ex1_05

--- B21_Ex01_5/Program.cs
+++ B21_Ex01_5/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace B21_Ex01_5
 {
     class Program
     {
+        public static void Main()
+        {
+            string userInput =  getValidInput();
+            //check input
+           
+            analyze(userInput);
+        }
+
+        private static string getValidInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void analyze(string i_Input)
+        {
+            // 1 - check the biggest digit
+            // 2 - check the smallest digit
+            // 3 - how many letters are divide by 3
+            // 4 - the number of digits that bigger then the unit digit
+            
+        }
     }
 }
