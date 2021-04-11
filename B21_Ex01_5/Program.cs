4ff6025bed0e9f9e7a06ef01b135f96a18cdebf8
Omri Cohen <omricohen101@gmail.com>
2021-04-11 15:19:31 +0300

    Ex1_05 biggest, smallst

--- B21_Ex01_5/Program.cs
+++ B21_Ex01_5/Program.cs
@@ -4,11 +4,26 @@ namespace B21_Ex01_5
 {
     class Program
     {
+        private static void analyzeString(string i_Input)
+        {
+            int higestDigit = int.Parse(i_Input[0].ToString());
+            int smallestDigit = int.Parse(i_Input[0].ToString());
+            int numberOfLettersDivideByThree = 0;
+            int lengthOfInput = i_Input.Length;
+            char currentDigit;
+            Console.WriteLine();
+            // 1 - check the biggest digit
+
+            // 2 - check the smallest digit
+            // 3 - how many letters are divide by 3
+            // 4 - the number of digits that bigger then the unit digit
+        }
+
         public static void Main()
         {
-            string userInput =  getValidInput();
+            string userInput = getValidInput();
             //check input
-           
+
             analyze(userInput);
         }
 
@@ -17,13 +32,33 @@ namespace B21_Ex01_5
             throw new NotImplementedException();
         }
 
-        private static void analyze(string i_Input)
+        private static int largetsDigit(int i_Number)
         {
-            // 1 - check the biggest digit
-            // 2 - check the smallest digit
-            // 3 - how many letters are divide by 3
-            // 4 - the number of digits that bigger then the unit digit
-            
+            int userInput = i_Number;
+            int largestDigit = i_Number % 10;
+
+            while(userInput > 0)
+            {
+                largestDigit = Math.Max(userInput % 10, largestDigit);
+                userInput /= 10;
+            }
+
+            return largestDigit;
         }
+
+        private static int smallestDigit(int i_Number)
+        {
+            int userInput = i_Number;
+            int largestDigit = i_Number % 10;
+
+            while(userInput > 0)
+            {
+                largestDigit = Math.Min(userInput % 10, largestDigit);
+                userInput /= 10;
+            }
+
+            return largestDigit;
+        }
+        
     }
 }
